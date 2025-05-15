e678f756fce638f0069d40bf0cb80de5e0236552
TobiasRequena <requenatobias3@gmail.com>
2025-05-15 12:11:08 -0300

    Guardando

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import { useGoogleLogin } from '@react-oauth/google';
+import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [userInfo, setUserInfo] = useState(null);
+
+  const login = useGoogleLogin({
+    onSuccess: async (tokenResponse) => {
+      console.log('Token de acceso:', tokenResponse);
+      try {
+        const res = await fetch('https://www.googleapis.com/oauth2/v3/userinfo', {
+          headers: {
+            Authorization: `Bearer ${tokenResponse.access_token}`,
+          },
+        });
+
+        const data = await res.json();
+        setUserInfo(data); // Guarda los datos del perfil
+      } catch (err) {
+        console.error('Error al obtener los datos del perfil:', err);
+      }
+    },
+    onError: (errorResponse) => console.error('Error de login:', errorResponse),
+  });
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button onClick={() => login()}>Sign in with Google</button>
+
+      {userInfo && (
+        <div>
+          <h2>Bienvenido, {userInfo.name}</h2>
+          <p>Email: {userInfo.email}</p>
+          <img src={userInfo.picture} alt="Foto de perfil" />
+        </div>
+      )}
+      {console.log(userInfo)}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
